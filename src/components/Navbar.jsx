30e144d610b29f0bc9f1a0fd5a3f8b1d2728ae05
BootstrapFootkicker <ksealy93@gmail.com>
2024-04-22 17:26:19 -0400

    placeholder navbar
    Created basic navbar
    Replaced default styling for index

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import "../styles/Navbar.css";
+
+export function Navbar() {
+  return (
+    <>
+      <div className={"navbar"}>
+        <div className="homeLogoDiv">
+          <div className={"navbarLogo"}>
+            <img src="../assets/logo.png" alt="Boot Logo" className="logo" />
+          </div>
+          <button className="navButton">Home</button>
+        </div>
+
+        <div className="cartDiv">
+          <button className="cartButton">Cart</button>
+        </div>
+      </div>
+    </>
+  );
+}
